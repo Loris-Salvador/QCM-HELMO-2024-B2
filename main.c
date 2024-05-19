01b13c8e163013643499727d73718ababb00e5d9
Loris Salvador <dev.loris.salvador@gmail.com>
2024-05-19 15:52:53 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,105 +4,113 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include "lecture/lecture.h"
+#include "lecture.h"
+//#include "..\..\Modules\Chaine 2(S17)\lecture.h"// bibliotheque du prof fonction du prof
 
-#define DELIMITEUR ';'
-#define LGMAXLIGNE 512
+#define DELIMITEUR ';' //delimiteur que l'on peut changer si jms on a des , on change
+#define LGMAXLIGNE 512 // nbr max lignes fichier
 
-char* RecupererToken(char chaine[], char delimiteur);
-int ExtraireInformations(char ligne[], char** question, int* nbPropositions, char*** propositions, float** points);
-char ** ExtraireFichierEnTableau(char nomFichier[], int* nbLigne);
+// en tete fonction
+char* RecupererToken(char chaine[], char delimiteur);//fonction qui renvoie un pointeur vers une chaine de caractere,prend une chaine jusqu'au premiere delimiteur 
+//si on veut appeler toute la ligne on doit l'appeler autant de fois qu'on en a besoin.
+int ExtraireInformations(char ligne[], char** question, int* nbPropositions, char*** propositions, float** points); //char*** est la pour ressortir un tableau a double entrée 
+char** ExtraireFichierEnTableau(char nomFichier[], int* nbLigne);
 int TirageAleatoire(int maximum, int tab[], int nbElements);
 void FreeTabQcm(char** tabQcm, int nbLignes);
 float RechercherMaximum(float tab[], int nbElements);
+void PermutationNEntier(int *tab, int n);
 
 
 int main() {
 
-    srand(time(NULL));
+    srand(time(NULL));// chaque execution avoir tirage aleatoire
 
     int nbQuestions, nbLignes, numLigne, reponse, nbPropositions, retour, i;
     int questionsPosees = 0;
-    int* tabQuestionsPosees;
+    int* tabQuestionsPosees; // 
     char* question;
     char* nomFichier;
-    char** propositions;
-    float* points;
+    char** propositions; //pointeur de pointeur d'une chaine de caractere
+    float* points; //ini d'un pointeur de float une fonction plus loin 
     float total = 0;
     float maxPoints = 0;
     char rejouer;
 
-    nomFichier = LireChaineDynamique("Entrez le nom du fichier : ");
+    nomFichier = LireChaineDynamique("Entrez le nom du fichier : "); //nom fichier il y aura ce qu'on a envoyer dans terminal et donc le chemin vers le fichier
 
-    char** tabQcm = ExtraireFichierEnTableau(nomFichier, &nbLignes);
-    free(nomFichier);
-    if(tabQcm == NULL)
+    char** tabQcm = ExtraireFichierEnTableau(nomFichier, &nbLignes); //tableau a deux dimension, lis tout le fichier et le mets dans tabqcm.Nous avons tout le fichier dedans
+    free(nomFichier); //vu que tout est dans tab qcm on n'a plus besoin du fihcier, meme si on rejoue on garde tabqcm
+    if (tabQcm == NULL)
     {
-        return 1;
+        return 1;//erreur dans extraire fichier tableau. 
     }
 
-    do
+    do //nous permet d'avoir acces a toute les infos dans l'excel du prof
     {
         do
         {
-            LireEntier("\nCombien de questions voulez-vous : ", &nbQuestions);
+            LireEntier("\nCombien de questions voulez-vous : ", &nbQuestions); //demander le nbr de question qu'on veut 
             if (nbQuestions > nbLignes)
             {
-                printf("Il n'y a pas assez de questions dans le fichier !");
+                printf("Il n'y a pas assez de questions dans le fichier !"); 
             }
-        } while (nbQuestions > nbLignes);
+        } while (nbQuestions > nbLignes); //tant que le nbr de question n'est pas bon 10 ca nous renvoie pas assez de question
 
-        tabQuestionsPosees = (int*)malloc(nbQuestions * sizeof(int));
+        tabQuestionsPosees = (int*)malloc(nbQuestions * sizeof(int)); //tableau de int qui sert à ne pas poser une question deux fois. et savoir ce qu'on a deja posé
         if (tabQuestionsPosees == NULL) {
-            FreeTabQcm(tabQcm, nbLignes);
+            FreeTabQcm(tabQcm, nbLignes); //liberer la memoire en cas de problemes 
             printf("Erreur d'allocation Tableau questions posees.\n");
             return 1;
         }
 
         do
         {
-            numLigne = TirageAleatoire(nbLignes, tabQuestionsPosees, nbQuestions);
+            numLigne = TirageAleatoire(nbLignes, tabQuestionsPosees, nbQuestions); //numligne == nouvelle question tirée
 
-            retour = ExtraireInformations(tabQcm[numLigne], &question, &nbPropositions, &propositions, &points);
-            if (retour == 1)
+            retour = ExtraireInformations(tabQcm[numLigne], &question, &nbPropositions, &propositions, &points); //  1er entre et 4autres sorties --> une fois passer dedans toutes variables sont initialise 
+            if (retour == 1) //si probleme on libere 
             {
                 free(tabQuestionsPosees);
                 FreeTabQcm(tabQcm, nbLignes);
                 return 1;
             }
 
-            tabQuestionsPosees[questionsPosees] = numLigne;
+            tabQuestionsPosees[questionsPosees] = numLigne; //on ajoute le numero de la ligne au tableau de questions deja posées car on veut plus retombé dessus
 
-            printf("\nQuestion: %s\n", question);
+            printf("\nQuestion: %s\n", question); 
+
+            int* tabOrdreProposition = (int*)malloc(nbPropositions * sizeof(int));
+
+            PermutationNEntier(tabOrdreProposition, nbPropositions);
 
             printf("\nPropositions :\n");
             for (i = 0; i < nbPropositions; i++) {
-                printf("%d : %s\n", i + 1, propositions[i]);
+                printf("%d : %s\n", i + 1, propositions[tabOrdreProposition[i]]); //%s imprime les chaines de caracter == les propositions
             }
 
             do {
                 LireEntier("\nVotre Reponse : ", &reponse);
-                if (reponse > nbPropositions || reponse < 1)
+                if (reponse > nbPropositions || reponse < 1) // || ==OU
                 {
                     printf("Aucune propositions ne correspond");
                 }
             } while (reponse > nbPropositions || reponse < 1);
 
-            total = total + points[reponse - 1];
-            maxPoints = maxPoints + RechercherMaximum(points, nbPropositions);
+            total = total + points[reponse - 1]; //-1 car tableau commence a 0 
+            maxPoints = maxPoints + RechercherMaximum(points, nbPropositions); 
 
-            free(question);
-            for (i = 0; i < nbPropositions; i++) {
-                free(propositions[i]);
+            free(question); //on libere tout pour la prochaine question 
+            for (i = 0; i < nbPropositions; i++) { // boucle car plrs propo dans le tableau 
+                free(propositions[i]); //liberer les elements du tableau
             }
-            free(propositions);
+            free(propositions); // libere le tableau 
             free(points);
 
             questionsPosees++;
         } while (questionsPosees < nbQuestions);
 
 
-        printf("\nVotre Total : %.2f/%.2f", total, maxPoints); // est-ce que le total peut etre negatif? si non il faudra changer (un bete if(total < 0) total = 0;)
+        printf("\nVotre Total : %.2f/%.2f", total, maxPoints); 
 
         LireCaractere("\n\nVoulez vous rejouer? (O/N)", &rejouer);
 
@@ -111,67 +119,65 @@ int main() {
             total = 0;
             questionsPosees = 0;
             maxPoints = 0;
-            system("cls");
+            system("cls"); //clear le terminal 
         }
 
-        free(tabQuestionsPosees);
+        free(tabQuestionsPosees); // on peut reutilise les questions qu'on a utiliser dans le 1er jeu pour le prochian
 
     } while (rejouer == 'O');
 
-    FreeTabQcm(tabQcm, nbLignes);
+    FreeTabQcm(tabQcm, nbLignes); 
 
     return 0;
 }
 
-char* RecupererToken(char chaine[], char delimiteur) {
+char* RecupererToken(char chaine[], char delimiteur) { //deux entrees chaine et delimiteur, une chaine et le delimiteur et la sortie est debut chaine jusqu'au delimeur 
     int i = 0;
     while (chaine[i] != delimiteur && i < strlen(chaine)) {
-        i++;
+        i++; //compte le nbr de caractere jusqu'au 1er delimiteur. && est la pour s'arreter a la fin de la chaine
     }
 
-    char* token = (char*)malloc((i + 1) * sizeof(char));
-    if (token == NULL) {
+    char* token = (char*)malloc((i + 1) * sizeof(char));//allocation dynamique car apres etre passer dans la chaine on sait le nbr de caractere, malloc== fonction
+    if (token == NULL) { //si probleme avec malloc ca nous renvoie rien, token c'est ce qu'on va renvoyer(token== chaine jusqu'au delimiteur) 
         return NULL;
     }
 
-    strncpy(token, chaine, i);
-    token[i] = '\0';
+    strncpy(token, chaine, i);//token== destination, chaine==source(on met chaine dans token), on met que les i premiers dans token
+    token[i] = '\0'; //tu finis ta chaine par un \0
     return token;
 }
 
-int ExtraireInformations(char ligne[], char** question, int* nbPropositions, char*** propositions, float** points) {
-    char* ptrLigne = ligne;
+int ExtraireInformations(char ligne[], char** question, int* nbPropositions, char*** propositions, float** points) { //extraire ce qu'il y a dans ligne dans chauqe variable
+    char* ptrLigne = ligne; //pointe vers le premier element de la ligne
 
-    *question = RecupererToken(ptrLigne, DELIMITEUR);
+    *question = RecupererToken(ptrLigne, DELIMITEUR); //on lui passe toute la ligne, et dans question on met tout ce qui a avant le premier; 
     if (question == NULL) {
         printf("Erreur d'allocation Token question.\n");
         return 1;
     }
 
-    ptrLigne = ptrLigne + strlen(*question) + 1;
+    ptrLigne = ptrLigne + strlen(*question) + 1;//on avance le pointeur jusqu'au premier ; et on va au 2eme ;
 
-    char* nbPropStr = RecupererToken(ptrLigne, DELIMITEUR);
-    int nbProp;
-    sscanf(nbPropStr, "%d", &nbProp);
-    *nbPropositions = nbProp;
+    char* nbPropStr = RecupererToken(ptrLigne, DELIMITEUR); //recup token renvoie des char*
+    sscanf(nbPropStr, "%d", nbPropositions); //on veut pas des char mais des int. On recupere le nbr de propositon en char et on les transforme avec sscanf pour avoir int
+   
 
-    ptrLigne = ptrLigne + strlen(nbPropStr) + 1;
+    ptrLigne = ptrLigne + strlen(nbPropStr) + 1; //on avance le pointeur jusqu'au 2eme ; et on va au 3eme ;
+    free(nbPropStr); //libere une fois que l'on a converti le char en int
 
-    free(nbPropStr);
-
-    *propositions = (char**)malloc(nbProp * sizeof(char*));
+    *propositions = (char**)malloc(*nbPropositions * sizeof(char*));//on alloue pour le propositon,pointeur vers tableau de pointeur et malloc va donc alloue pour le char de nbr de proposition
     if (question == NULL) {
-        free(*question);
+        free(*question); //si double pointeur on asse une *
         printf("Erreur d'allocation propositions.\n");
         return 1;
     }
 
-    for (int i = 0; i < nbProp; i++) {
-        (*propositions)[i] = RecupererToken(ptrLigne, DELIMITEUR);
-        ptrLigne = ptrLigne + strlen((*propositions)[i]) + 1;
+    for (int i = 0; i < *nbPropositions; i++) {//on a trouvé les proposition dans la ligne et on va remplir le tableau de propostions
+        (*propositions)[i] = RecupererToken(ptrLigne, DELIMITEUR); //determiner ordre prio pour les () du *propo, ici on veut l'element i de la valeur de propositions si on met pas on a la valeur de l'element i de proposionts
+        ptrLigne = ptrLigne + strlen((*propositions)[i]) + 1;  //on met chaque element a sa place //on veut la valeur et non pas l'adresse(positions exacte dans pc) alord que valeur== ce qu'il y a dans l'adresse
     }
 
-    *points = (float*)malloc(nbProp * sizeof(float));
+    *points = (float*)malloc(*nbPropositions * sizeof(float)); //alloue nbpropositon x la taille d'un float 
     if (question == NULL) {
         free(*propositions);
         free(*question);
@@ -179,79 +185,81 @@ int ExtraireInformations(char ligne[], char** question, int* nbPropositions, cha
         return 1;
     }
 
-    for (int i = 0; i < nbProp; i++) {
-        char* tmp = RecupererToken(ptrLigne, DELIMITEUR);
-        sscanf(tmp, "%f", &(*points)[i]);
-        ptrLigne = ptrLigne + strlen(tmp) + 1;
+    for (int i = 0; i < *nbPropositions; i++) { //rempli dans un tableau les points de la ligne
+
+        char* tmp = RecupererToken(ptrLigne, DELIMITEUR); //recuperer dans tampon les points dans chaque tour de boucle
+        sscanf(tmp, "%f", &(*points)[i]); //recuperer dans un char et le transformer en float et le mettre dans tableau de points 
+        ptrLigne = ptrLigne + strlen(tmp) + 1;//avancer le pointeur
         free(tmp);
     }
 
     return 0;
 }
 
-char ** ExtraireFichierEnTableau(char nomFichier[], int* nbLigne)
+char** ExtraireFichierEnTableau(char nomFichier[], int* nbLigne) //nom fichier ce qu'on a rentrer dans terminal, main envoie adresse de nblignes comme ca la fonction peut modif nbr de ligne
 {
-    FILE* fp;
-    char** tab;
-    char ligne[LGMAXLIGNE];
+    FILE* fp; //pointeur vers un fichier (fichier excel), se deplace dedans
+    char** tab; //ce qu'on renvoie a la fin de la fonction, tout le tableau
+    char ligne[LGMAXLIGNE];//ou on stocke une ligne dans le fihcier;lgmax car on sait pas nbr de caractere dans une ligne du fichier 
 
-    fp = fopen(nomFichier, "r");
-    if (fp == NULL)
+    fp = fopen(nomFichier, "r"); //ouvre le fichier en mode lecture, fp va se mettre dans le premiere caractere du fichier
+    if (fp == NULL) // probleme car pas fp au debur du fichier
     {
         printf("Impossible d'ouvrir le fichier !\n");
         return NULL;
     }
 
-    *nbLigne = 0;
-    while(fgets(ligne, LGMAXLIGNE + 1, fp) != NULL)
-    {
-        (*nbLigne)++;
+    *nbLigne = 0; //modifier valeur de nb lignes et pas son adresse car*
+    while (fgets(ligne, LGMAXLIGNE + 1, fp) != NULL)//sert a compter le nombre de lignes, il se mettra a la ligne suivante grace au lignemax +1, fp va parcourir le fichier
+    {                                                  // copie ligne du fichier dans la variable ligne. 
+        (*nbLigne)++;                                  
     }
 
-    tab = malloc((*nbLigne) * sizeof(char*));
-    if (tab == NULL)
+    tab = malloc((*nbLigne) * sizeof(char*)); //allouer autant de places que l'on a de ligne, allouer nb ligne *de la place pour des pointeurs de char. 
+    if (tab == NULL)//probleme                // pointeur de chaine de caractere
     {
         printf("Erreur d'allocation tableau\n");
         return NULL;
     }
 
-    rewind(fp);
-    int i = 0;
+    rewind(fp);//une fois tout lu,la fct rewind remets fp au debut pour le relire et ne pas avoir de tabqcm vide. 
+    int i = 0; //adresse du premier element, i parcours le tableau de pointeur. 
 
-    while(fgets(ligne, LGMAXLIGNE + 1, fp) != NULL)
+    while (fgets(ligne, LGMAXLIGNE + 1, fp) != NULL) //chaque element du tab va etre allouer a une zone de memoire
     {
-        tab[i] = malloc((strlen(ligne) + 1) * sizeof(char));
+        tab[i] = malloc((strlen(ligne) + 1) * sizeof(char)); // on alloue une chaine de caractere == la ligne du tab, 
         if (tab[i] == NULL)
         {
-            free(tab);
+            free(tab); // liberer le tab si on a une erreur de malloc
             printf("Erreur d'allocation ligne\n");
             return NULL;
         }
-        strcpy(tab[i], ligne);
-        tab[i][strlen(ligne)] = '\0';
+        strcpy(tab[i], ligne);//dans tab i on a alloué de la place donc on peut copier la ligne dans tab i.
+        tab[i][strlen(ligne)] = '\0'; //mettre le \0 a la fin de la ligne
         i++;
     }
 
-    fclose(fp);
-    return tab;
+    fclose(fp); //femeture du fichier Excel 
+    return tab; // tout ce qu'on a fait dans la fonction on le renvoie dans tab. 
 }
 
-int TirageAleatoire(int maximum, int tab[], int nbElements) {
-    int retry, nb;
+int TirageAleatoire(int maximum, int tab[], int nbElements) { //maximum== nbr de ligne max ici 10, tab== tableau de question deja posées, nbElement == cbm element dans tab, (1;3;4;...)
+    int retry, nb; //si on tombe sur question deja poser
 
     do
     {
         retry = 0;
-        nb =  rand() % maximum;
+        nb = rand() % maximum; // je ne veux pas un nbr plus gd que le max==10, si max==10 nbr peut pas etre plus grand que 9
 
-        for(int i = 0; i < nbElements; i++)
+        for (int i = 0; i < nbElements; i++) //parcours chaque element du tableau
         {
-            if(nb == tab[i])
+            if (nb == tab[i]) //si le nb est egale a un element du tableau qu'on a deja tire alors retry==1
             {
                 retry = 1;
+                break; //sort du for car c'est sur qu'on va recommencer
             }
         }
-    } while (retry == 1);
+    } while (retry == 1); // des qu'on rentre dans le if retry ==1 donc refais le do while tant que retry n'est pas different de 1. Des qu'il est ==0 on sort de la boucle et nouvelle question est posé
 
     return nb;
 }
@@ -259,22 +267,37 @@ int TirageAleatoire(int maximum, int tab[], int nbElements) {
 void FreeTabQcm(char** tabQcm, int nbLignes)
 {
     for (int i = 0; i < nbLignes; i++) {
-        free(tabQcm[i]);
+        free(tabQcm[i]); //liberer element tableau 
     }
-    free(tabQcm);
+    free(tabQcm);//liberer le tableau 
 }
 
-float RechercherMaximum(float tab[], int nbElements)
+float RechercherMaximum(float tab[], int nbElements)  // parcours le tableau pour trouver le max et donc trouver le deno des points /...
 {
-    float max = tab[0];
+    float max = tab[0]; 
 
-    for(int i = 1; i<nbElements; i++)
+    for (int i = 1; i < nbElements; i++)
     {
-        if(tab[i] > max)
+        if (tab[i] > max)
         {
             max = tab[i];
         }
     }
 
     return max;
+}
+
+void PermutationNEntier(int *tab, int n) {
+    for (int i = 0; i < n; i++) {
+        tab[i] = i;
+    }
+
+    int tmp;
+
+    for (int i = n - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        tmp = tab[i];
+        tab[i] = tab[j];
+        tab[j] = tmp;
+    }
 }
